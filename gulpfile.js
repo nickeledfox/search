7cfe7138cd3b334f185b27bb6d2312962e7ceff7
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2022-03-14 21:28:38 -0700

    Configured project structure

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, watch, parallel } = require('gulp');
+const pug = require('gulp-pug');
+const less = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+const browsersync = () => {
+  browserSync.init({
+    server: {
+      baseDir: 'dist/',
+    },
+    port: 3000,
+    notify: false,
+  });
+};
+
+const pughtml = () => {
+  return src(['src/views/**/*.pug', '!src/views/includes/**/*.pug'])
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream());
+};
+
+const styles = () => {
+  return src('src/less/main.less')
+    .pipe(
+      less({
+        includePath: ['./node_modules'],
+      })
+    )
+    .pipe(
+      autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })
+    )
+    .pipe(dest('dist/styles'))
+    .pipe(browserSync.stream());
+};
+
+const watching = () => {
+  watch(['src/less/**/*.less'], styles);
+  watch(['src/views/**/*.pug'], pughtml).on('change', browserSync.reload);
+};
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.pughtml = pughtml;
+
+exports.default = parallel(pughtml, styles, browsersync, watching);
